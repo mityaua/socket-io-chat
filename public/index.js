a6f9336b26e7f97dab2f45461a14958990269d05
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-07-09 17:32:10 +0300

    Added files from local space

---
+++ public/index.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+window.addEventListener("load", () => {
+  const username = prompt('Type your name', 'Anonym')
+
+  const socket = io();
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (input.value) {
+      socket.emit('CHAT_MESSAGE', { message: input.value, username })
+      input.value = ""
+    }
+  })
+
+  socket.on('CHAT_UPDATE', ({ message, username }) => {
+    const item = document.createElement('li')
+    item.innerHTML = `${username}: ${message}`
+    messages.appendChild(item)
+
+    window.scrollTo(0, document.body.scrollHeight)
+  })
+
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+require('dotenv').config()
+
+const port = process.env.PORT || 8088
+
+const app = express();
+const server = require('http').createServer(app);
+
+// Сокеты
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('CHAT_MESSAGE', ({ message, username }) => {
+    io.emit('CHAT_UPDATE', { message, username })
+  })
+});
+
+// Логер + статика
+app.use(morgan('tiny'))
+app.use(express.static("public"))
+
+// Тестовый маршрут для http сервера
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Слушаем порт
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
